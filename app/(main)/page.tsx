8262262c6d6dfef1ff3b4ec6f17456670b0472eb
Maru <71565617+them2dt@users.noreply.github.com>
2025-10-07 15:16:24 +0200

    update start-page to merge authentication in the root-page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,16 +2,31 @@
 import { Hero } from "@/components/hero";
 import { CTASection } from "@/components/cta-section";
 import { Footer } from "@/components/layout";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Store, Calendar,Trophy, Users, Network } from "lucide-react";
+import Backdrop from "@/components/ui/backdrop";
+import { useState } from "react";
+
+import AuthPanel from "@/components/ui/auth-panel";
 
 export default function Home() {
+  const [authPanel,setAuthPanel]=useState(false);
+  const [authType,setAuthType]=useState(false);// false=> login, true=> register
+  const openSignIn=()=>{setAuthType(false);setAuthPanel(true);};
+  const openSignUp=()=>{setAuthType(true);setAuthPanel(true);};
+  const closeAuth=()=>{setAuthPanel(false);};
   return (
-    <main className="w-screen h-screen flex flex-col items-center">
+    <main className="w-screen h-screen flex flex-col items-center bg-background">
+      {/* Theme Button - Absolute positioned */}
+      <div className="absolute top-4 right-4 z-50">
+        <ThemeSwitcher />
+      </div>
+      
       <div className="flex-1 w-full h-full flex flex-col px-60 py-32 items-center">
-        <div className="relative flex flex-row items-center justify-center w-full h-full border-[1px] border-gray-200 rounded-3xl overflow-hidden">
-          <div className="flex flex-col items-center justify-center gap-16 w-full h-full border-r-[1px] border-r-gray-200 bg-gray-100 rounded-l-3xl">
+        <div className="relative flex flex-row items-center justify-center w-full h-full border-[1px] border-foreground/10 rounded-3xl overflow-hidden">
+          <div className="flex flex-col items-center justify-center gap-16 w-full h-full bg-foreground/5 border-r-[1px] border-r-foreground/10 rounded-l-3xl">
             <div className="flex flex-col items-center justify-center gap-2 px-20">
-              <div className="font-heading font-semibold text-3xl">
+              <div className="font-heading font-semibold text-foreground text-3xl">
                 Synantica, the database for STEM.
               </div>
               <div className="font-sans font-regular text-center text-lg text-gray-500">
@@ -19,12 +34,12 @@ export default function Home() {
                 your potential and grow your network.
               </div>
             </div>
-            <div className="w-4/5 h-[1px] bg-gray-200"></div>
+            <div className="w-4/5 h-[1px] bg-foreground/10"></div>
             <div className="flex flex-row items-center justify-center gap-16">
               <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-gray-100 border-b-teal-100 border-t-teal-100 rounded-full">
+                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-foreground/5 border-b-teal-500/40 border-t-teal-500/40 rounded-full">
                   <div className="flex flex-row items-center justify-center h-full w-full bg-teal-500 rounded-full">
-                    <Trophy className="text-white" />
+                    <Trophy className="text-background" />
                   </div>
                 </div>
                 <div className="font-sans font-semibold text-xl text-foreground">
@@ -32,9 +47,9 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-gray-100 border-b-amber-100 border-t-amber-100 rounded-full">
+                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-foreground/5 border-b-amber-500/40 border-t-amber-500/40 rounded-full">
                   <div className="flex flex-row items-center justify-center h-full w-full bg-amber-500 rounded-full">
-                    <Users className="text-white" />
+                    <Users className="text-background" />
                   </div>
                 </div>
                 <div className="font-sans font-semibold text-xl text-foreground">
@@ -42,9 +57,9 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-gray-100 border-b-pink-100 border-t-pink-100 rounded-full">
+                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-foreground/5 border-b-pink-500/40 border-t-pink-500/40 rounded-full">
                   <div className="flex flex-row items-center justify-center h-full w-full bg-pink-500 rounded-full">
-                    <Network className="text-white" />
+                    <Network className="text-background" />
                   </div>
                 </div>
                 <div className="font-sans font-semibold text-xl text-foreground">
@@ -52,9 +67,9 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-gray-100 border-b-purple-100 border-t-purple-100 rounded-full">
+                <div className="flex flex-row items-center justify-center h-20 w-20 p-2 border-2 border-foreground/5 border-b-purple-500/40 border-t-purple-500/40 rounded-full">
                   <div className="flex flex-row items-center justify-center h-full w-full bg-purple-500 rounded-full">
-                    <Store className="text-white" />
+                    <Store className="text-background" />
                   </div>
                 </div>
                 <div className="font-sans font-semibold text-xl text-foreground">
@@ -63,21 +78,21 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="relative flex flex-col w-full h-full bg-pink-500 rounded-0">
+          <div className="relative flex flex-col w-full h-full rounded-0">
             <img
               src="/cover.png"
               alt="Synantica Cover"
               className="w-full h-full object-cover"
             />
             <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-row justify-center items-end p-0">
-              <div className="h-24 w-full flex flex-row justify-center items-center p-4 gap-2 backdrop-blur-sm bg-white/10 border-t-[1px] border-white/20">
-                <button className="flex flex-row items-center justify-center w-full h-full cursor-pointer bg-pink-500 border-[2px] border-black/20 rounded-lg"  onClick={()=>{window.location.href="/auth/login"}}>
-                  <div className="whitespace-nowrap font-sans font-bold text-white">
+              <div className="h-24 w-full flex flex-row justify-center items-center p-4 gap-2 backdrop-blur-sm bg-background/10 border-t-[1px] border-background/20">
+                <button className="flex flex-row items-center justify-center w-full h-full cursor-pointer bg-pink-500 border-[2px] border-foreground/20 rounded-lg" onClick={openSignIn}>
+                  <div className="backgroundspace-nowrap font-sans font-bold text-background">
                     Log in
                   </div>
                 </button>
-                <button className="flex flex-row items-center justify-center w-full h-full cursor-pointer bg-white border-[2px] border-black/20 rounded-lg">
-                  <div className="whitespace-nowrap font-sans font-bold text-black">
+                <button className="flex flex-row items-center justify-center w-full h-full cursor-pointer bg-background border-[2px] border-foreground/20 rounded-lg" onClick={openSignUp}>
+                  <div className="backgroundspace-nowrap font-sans font-bold text-foreground">
                     Register
                   </div>
                 </button>
@@ -86,6 +101,10 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {authPanel&&<Backdrop onClick={closeAuth}>
+        <AuthPanel mode={authType?"signup":"signin"} onClose={closeAuth}/>
+      </Backdrop>}
     </main>
   );
 }
---
+++ components/ui/auth-panel.tsx
@@ -0,0 +1,57 @@
+import { Clock, X } from "lucide-react";
+import React from "react";
+
+export default function AuthPanel({
+  mode = "signup",
+  onClose
+}: {
+  mode?: "signin" | "signup";
+  onClose: ()=>void;
+}) {
+  return (
+    <div onClick={(e)=>{e.stopPropagation();}} className="relative flex flex-col justify-center items-center p-8 gap-8 bg-background/80 border-[1px] border-background/20 rounded-xl ">
+
+      <div className="text-sans font-semibold text-foreground text-3xl">
+        {mode == "signin" ? "Welcome back!" : "Sign up now!"}
+      </div>
+      <div className="flex flex-col justify-center items-center gap-3 w-72">
+        <input
+          className="w-full h-12 p-4 border-[1px] border-foreground/20 rounded-lg text-lg"
+          placeholder="E-Mail"
+        />
+        <input
+          className="w-full h-12 p-4 border-[1px] border-foreground/20 rounded-lg text-lg"
+          placeholder="Password"
+        />
+        {mode == "signup" && (
+          <input
+            className="w-full h-12 p-4 border-[1px] border-foreground/20 rounded-lg text-lg"
+            placeholder="Confirm password"
+          />
+        )}
+        {mode == "signup" ? (
+          <input
+            className="flex flex-col justify-center items-center w-full h-12 bg-foreground border-[1px] border-foreground/20 rounded-lg font-heading tracking-wide font-semibold text-background text-lg"
+            type="submit"
+            placeholder="Password"
+            value="Sign up"
+          />
+        ) : (
+          <input
+            className="flex flex-col justify-center items-center w-full h-12 bg-foreground border-[1px] border-foreground/20 rounded-lg font-heading tracking-wide font-semibold text-background text-lg"
+            type="submit"
+            placeholder="Password"
+            value="Log in"
+          />
+        )}
+        <div className="flex flex-row justify-center items-center gap-2">
+          <input className="" type="checkbox" placeholder="Password" />
+          <div className="font-sans font-regular text-sm text-foreground">
+            Remember me
+          </div>
+        </div>
+      </div>
+      <button className="absolute top-2 right-2 flex flex-row justify-center items-center" onClick={onClose}><X/></button>
+    </div>
+  );
+}
